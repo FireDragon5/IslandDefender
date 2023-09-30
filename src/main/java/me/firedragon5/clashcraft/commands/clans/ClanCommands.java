41283496fd40571cf55a2ff2603be8fe514a0137
Anthonie Slabbert <20230585@ctucareer.co.za>
2023-09-30 09:46:01 +0200

    Update the class to make it more readable

--- src/main/java/me/firedragon5/clashcraft/commands/clans/ClanCommands.java
+++ src/main/java/me/firedragon5/clashcraft/commands/clans/ClanCommands.java
@@ -1,7 +1,5 @@
 package me.firedragon5.clashcraft.commands.clans;
 
-import me.firedragon5.clashcraft.filemanager.clans.ClanFolderManager;
-import me.firedragon5.clashcraft.filemanager.player.PlayerFileManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,36 +29,30 @@ public class ClanCommands implements CommandExecutor, TabCompleter {
 
 //		Permissions for clan commands
 		if (args[0].equalsIgnoreCase("create")) {
-			if (sender.hasPermission("clashcraft.clan.create")) {
-
-				ClanFolderManager.getFileManager().addClan(player ,args[1], args[2]);
-				PlayerFileManager.setPlayerClanName(player, args[1]);
 
 
-			} else {
-				player.sendMessage( "&cYou do not have permission to use this command!");
-			}
-			return true;
+			CreateCommand.createClan(player, args[1], args[2]);
+
+
 		} else if (args[0].equalsIgnoreCase("join")) {
-			if (sender.hasPermission("clashcraft.clan.join")) {
 
-				ClanFolderManager.getFileManager().joinClan(args[1], player);
-
-				PlayerFileManager.setPlayerClanName(player, args[1]);
-
-			}else {
-				player.sendMessage("&cYou do not have permission to use this command!");
-			}
+			JoinCommand.joinClan(player, args[1]);
 
 
 		}else if (args[0].equalsIgnoreCase("leave")) {
-			if (sender.hasPermission("clashcraft.clan.leave")) {
 
-				ClanFolderManager.getFileManager().leaveClan(player);
+			LeaveCommand.leaveClan(player, args[1]);
+		}else if (args[0].equalsIgnoreCase("invite")) {
 
-			} else {
-				player.sendMessage( "&cYou do not have permission to use this command!");
-			}
+//			InviteCommand.invitePlayer(player, args[1], args[2]);
+		}else if (args[0].equalsIgnoreCase("kick")) {
+
+//			KickCommand.kickPlayer(player, args[1], args[2]);
+
+
+		}else if (args[0].equalsIgnoreCase("list")) {
+
+			ListCommand.listClans(player);
 		}
 
 
@@ -71,61 +63,57 @@ public class ClanCommands implements CommandExecutor, TabCompleter {
 	@Override
 	public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
-		List <String> tabComplete = new ArrayList<>();
+		List<String> tabComplete = new ArrayList<>();
 
+//		Commands all the player has: /clan info|list|help|join|leave|create(Needs permission)
+//		Commands only the leader has: /clan invite|kick|promote|demote|disband
+//		Commands only the admin has: /clan delete|setleader|setadmin|setmember
 
-			// create <clanName> <clanTag>
-			if (strings.length == 1) {
+		if (strings.length == 1) {
+			tabComplete.add("info");
+			tabComplete.add("list");
+			tabComplete.add("help");
+			tabComplete.add("join");
+			tabComplete.add("leave");
+			tabComplete.add("create");
+			if (commandSender.hasPermission("clashcraft.clan.create")) {
 				tabComplete.add("create");
-				tabComplete.add("join");
-				tabComplete.add("leave");
-				tabComplete.add("invite");
-				tabComplete.add("kick");
-				tabComplete.add("promote");
-				tabComplete.add("demote");
-				tabComplete.add("disband");
-				tabComplete.add("info");
-				tabComplete.add("list");
-				tabComplete.add("help");
-			} else if (strings.length == 2) {
-				if (strings[0].equalsIgnoreCase("create")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("join")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("leave")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("invite")) {
-					tabComplete.add("<playerName>");
-				} else if (strings[0].equalsIgnoreCase("kick")) {
-					tabComplete.add("<playerName>");
-				} else if (strings[0].equalsIgnoreCase("promote")) {
-					tabComplete.add("<playerName>");
-				} else if (strings[0].equalsIgnoreCase("demote")) {
-					tabComplete.add("<playerName>");
-				} else if (strings[0].equalsIgnoreCase("disband")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("info")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("list")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("help")) {
-					tabComplete.add("<clanName>");
-				}
-			} else if (strings.length == 3) {
-				if (strings[0].equalsIgnoreCase("create")) {
-					tabComplete.add("<clanTag>");
-				} else if (strings[0].equalsIgnoreCase("join")) {
-					tabComplete.add("<clanTag>");
-				} else if (strings[0].equalsIgnoreCase("leave")) {
-					tabComplete.add("<clanTag>");
-				} else if (strings[0].equalsIgnoreCase("invite")) {
-					tabComplete.add("<clanName>");
-				} else if (strings[0].equalsIgnoreCase("kick")) {
-					tabComplete.add("<clanName>");
-				}
 			}
-
-
+			if (commandSender.hasPermission("clashcraft.clan.join")) {
+				tabComplete.add("join");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.leave")) {
+				tabComplete.add("leave");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.invite")) {
+				tabComplete.add("invite");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.kick")) {
+				tabComplete.add("kick");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.promote")) {
+				tabComplete.add("promote");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.demote")) {
+				tabComplete.add("demote");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.disband")) {
+				tabComplete.add("disband");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.delete")) {
+				tabComplete.add("delete");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.setleader")) {
+				tabComplete.add("setleader");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.setadmin")) {
+				tabComplete.add("setadmin");
+			}
+			if (commandSender.hasPermission("clashcraft.clan.setmember")) {
+				tabComplete.add("setmember");
+			}
+			return tabComplete;
+		}
 		return tabComplete;
 	}
 }
