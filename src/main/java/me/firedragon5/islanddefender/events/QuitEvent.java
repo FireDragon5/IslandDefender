07485cf7ef43885925181af4519fb3f3659a26a4
Anthonie Slabbert <20230585@ctucareer.co.za>
2023-11-05 18:34:10 +0200

    Updated the quit event

--- src/main/java/me/firedragon5/islanddefender/events/QuitEvent.java
+++ src/main/java/me/firedragon5/islanddefender/events/QuitEvent.java
@@ -1,6 +1,8 @@
 package me.firedragon5.islanddefender.events;
 
+import me.firedragon5.islanddefender.IslandDefender;
 import me.firedragon5.islanddefender.Utils;
+import me.firedragon5.islanddefender.instance.Cosmetic;
 import me.firedragon5.islanddefender.manager.NameTagManager;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
@@ -12,6 +14,13 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import java.util.Objects;
 
 public class QuitEvent implements Listener {
+
+
+	private final IslandDefender plugin;
+
+	public QuitEvent(IslandDefender plugin) {
+		this.plugin = plugin;
+	}
 
 	@EventHandler
 	public static void onLeave(PlayerQuitEvent event) {
@@ -26,6 +35,21 @@ public class QuitEvent implements Listener {
 				.getCachedData()
 				.getMetaData()
 				.getPrefix();
+
+
+//		Remove player from a vanished list
+		IslandDefender.getInstance().vanished.remove(player.getUniqueId());
+
+//		Remove player from pending friend requests
+		IslandDefender.pendingFriendRequests.remove(player);
+
+//		Remove player from active cosmetics
+		if (IslandDefender.getInstance().getActiveCosmetics().containsKey(player.getUniqueId())) {
+			for (Cosmetic cosmetic : IslandDefender.getInstance().getActiveCosmetics().get(player.getUniqueId())) {
+				cosmetic.disable();
+			}
+		}
+
 
 		event.setQuitMessage(Utils.chat("&c&l- " + prefix + "&c" + player.getName()));
 	}
